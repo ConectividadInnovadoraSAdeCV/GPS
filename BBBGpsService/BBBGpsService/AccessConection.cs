091c5858cba4033c91d5eab63f7e252b50bd9d00
Francisco <francisco@weon.mx>
2016-02-23 15:07:55 -0600

    Software GPS equipos conectividad

---
+++ BBBGpsService/BBBGpsService/AccessConection.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace RPCandle
+{
+    class AccessConeccion
+    {
+        public SqlConnection coneccion;
+        public string ConeccionString;
+        private float iva;
+        public AccessConeccion()
+        {
+            //*
+            ConeccionString = "Data Source = weon;initial catalog=weon; user id= RPCandle; password = Rosario13";
+            /*/
+            ConeccionString = "Data Source = localhost;initial catalog=weon; user id= RPCandle; password = Rosario13";
+            //*/
+            
+            coneccion = new SqlConnection(ConeccionString);
+        }
+        private bool AbrirConeccion()
+        {
+            try
+            {
+                coneccion.Open();
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show(ex.Message.ToUpper());
+                return false;
+            }
+            return true;
+        }
+        public bool CerrarConeccion()
+        {
+            try
+            {
+                coneccion.Close();
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show(ex.Message.ToUpper());
+                return false;
+            }
+            return true;
+        }
+        public DataTable ObtieneTabla(string Comando)
+        {
+            DataTable value;
+            value = new DataTable();
+            SqlDataAdapter da;
+
+            try
+            {
+                da = new SqlDataAdapter(Comando, coneccion);
+                da.Fill(value);
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show(ex.Message.ToUpper());
+            }
+
+            return value;
+        }
+        public DataRow GetFirstRow(string Comando)
+        {
+            DataTable dt = ObtieneTabla(Comando);
+            if (dt == null)
+                return null;
+            DataRow r;
+            try
+            {
+                r = dt.Rows[0];
+            }
+            catch (Exception)
+            {
+                //MessageBox.Show("No hay datos");
+                return null;
+            }
+            return r;
+        }
+        public Object ExecutaEscalar(string Comando)
+        {
+            Object value;
+            SqlCommand cmd;
+            value = null;
+            try
+            {
+                if (!AbrirConeccion())
+                    return value;
+                cmd = new SqlCommand(Comando, coneccion);
+                value = cmd.ExecuteScalar();
+                if (value == null)
+                    value = 0;
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show(ex.Message.ToUpper());
+                value = null;
+            }
+            finally
+            {
+                CerrarConeccion();
+            }
+
+            return value;
+        }
+
+        public string ToFecha(DateTime _Fecha)
+        {
+            string fecha = "NULL";
+            DateTime f = _Fecha;
+            string m = '0' + f.Month.ToString(); ;
+            string d = '0' + f.Day.ToString();
+            string h = '0' + f.Hour.ToString();
+            string mm = '0' + f.Minute.ToString();
+
+            m = m.Substring(m.Length - 2);
+            d = d.Substring(d.Length - 2);
+            h = h.Substring(h.Length - 2);
+            mm = mm.Substring(mm.Length - 2);
+
+            
+
+            if (_Fecha != null && _Fecha != DateTime.MinValue)
+            {
+                fecha = "CONVERT(DATETIME,'" + f.Year+ "-" + f.Month + "-" + f.Day  + " " + f.Hour + ":" + f.Minute + "',102)";
+            }
+            return fecha;
+        }
+
+        public float  Iva
+        {
+            get
+            {
+                string ObtieneIva = "SELECT Iva FROM [ct].[Iva]";
+                string aux = "";
+                aux = ExecutaEscalar(ObtieneIva).ToString();
+                iva = 0;
+                aux = aux.Replace(",", ".");
+                float.TryParse(aux, out iva);
+                return iva;
+            }
+            set
+            {
+                string NuevoIva = "INSERT INTO [ct].[Iva] (Iva, Fecha) VALUES (" + value + ", " + ToFecha(DateTime.Today) + ")";
+                ExecutaEscalar(NuevoIva);
+            }
+        }
+
+        public SqlDataReader GetDataReader(string Comando)// TODA VEZ QUE SE OBTIENE UN DATA READER SE MANTIENE LA CONECCION ABIERTA DEBE CERRARSE
+        {
+            SqlDataReader dr;
+            SqlCommand cmd;
+            dr = null;
+            try
+            {
+                AbrirConeccion();
+                cmd = new SqlCommand(Comando, coneccion);
+                dr = cmd.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show(ex.Message.ToUpper());
+            }
+            return dr;
+        }
+        public void ActualizaTabla(string cmdSelect, DataTable Tabla)
+        {
+            SqlDataAdapter da = new SqlDataAdapter(cmdSelect, coneccion);
+            SqlCommandBuilder cb = new SqlCommandBuilder(da);
+            da.Update(Tabla);
+        }
+
+    }
+}
+
---
+++ BBBGpsService/BBBGpsService/BBBGps.asmx.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+
+using RPCandle;
+
+namespace BBBGpsService
+{
+    /// <summary>
+    /// Summary description for BBBGps
+    /// </summary>
+    [WebService(Namespace = "http://weon.mx/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class BBBGps : System.Web.Services.WebService
+    {
+
+        [WebMethod(Description = "Recibe las coordenadas actuales")]
+        //*/
+        public bool SendCoordenates(string Camion, double Latitud, double Longitud, string Pasajeros, string FechaHora, string Pwd)
+        {
+            string lat, lon;
+            lat = Latitud.ToString();
+            lon = Longitud.ToString();
+            lat = lat.Replace(",", ".");
+            lon = lon.Replace(",", ".");
+            if (Pwd != "weonrules")
+            {
+                throw new Exception("No eres un usuario autorizado");
+            }
+            AccessConeccion ac = new AccessConeccion();
+            string cmd = "INSERT INTO gps.Coordenadas " +
+                                "(Camion,Latitud,Longitud,Pasajeros,Fecha) " +
+                         "VALUES " +
+                                "('" + Camion + "'," + lat + "," + lon + ",'" + Pasajeros + "',CAST('" + FechaHora + "' AS DATETIME))";
+            return ac.ExecutaEscalar(cmd) != null;
+
+        }
+        [WebMethod(Description = "Recibe mesages del sitema")]
+        public bool SendMesages(string Camion, string FechaHora,int Mesg,string Pwd)
+        {
+            if (Pwd != "weonrules")
+            {
+                throw new Exception("No eres un usuario autorizado");
+            }
+            AccessConeccion ac = new AccessConeccion();
+            string cmd = "INSERT INTO [dbo].[MensagesInternos] "+
+                                    "([Fecha],[Camion],[Mensage]) " +
+                         "VALUES " +
+                                "(CAST('" + FechaHora + "' AS DATETIME),'" + Camion + "'," + Mesg + ")";
+            return ac.ExecutaEscalar(cmd) != null;
+
+        }
+        /*/
+        public string SendCoordenates(string Camion, double Latitud, double Longitud, string Pasajeros, string FechaHora, string Pwd)
+        {
+            string lat, lon;
+            lat = Latitud.ToString();
+            lon = Longitud.ToString();
+            lat = lat.Replace(",", ".");
+            lon = lon.Replace(",", ".");
+            if (Pwd != "weonrules")
+            {
+                throw new Exception("No eres un usuario autorizado");
+            }
+            AccessConeccion ac = new AccessConeccion();
+            string cmd = "INSERT INTO gps.Coordenadas " +
+                                "(Camion,Latitud,Longitud,Pasajeros,Fecha) " +
+                         "VALUES " +
+                                "('" + Camion + "'," + lat + "," + lon + ",'" + Pasajeros + "',CAST('" + FechaHora + "' AS DATETIME))";
+            //return ac.ExecutaEscalar(cmd) != null;
+            return cmd;                    
+        }
+        //*/
+    }
+}
